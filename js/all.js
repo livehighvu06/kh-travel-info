6e841f4390a66fb04d31516c48e0f9c45cdeac1a
Eddie <livehighvu06@gmail.com>
2020-04-06 17:28:34 +0800

    修改hot-btn border:none /新增area-name使其對其area-location-img下方，並加上背景色/修改area-loacation-free，data裡面有免費參觀才顯示

--- js/all.js
+++ js/all.js
@@ -7,8 +7,8 @@ function addToList(){
     var str = '';
     var zone = []; 
     str+='<option>--請選擇行政區--</option>';
-    //從陣列篩選出不重複的值
     for(var i = 0;i<data.length;i++){
+        //從陣列篩選出不重複的值
         if(zone.indexOf(data[i].Zone)==-1){
             zone.push(data[i].Zone);
             str+='<option>'+data[i].Zone+'</option>';
@@ -22,25 +22,32 @@ function createCard(e){
     var location = e.target.value;
     var str = '';
     if(location =='--請選擇行政區--'){return;}
-    console.log(location);
     for(var i=0;i < data.length;i++){
         var url = data[i].Picture1;
         url = url.replace("http","https");
         if(data[i].Zone == location){
+            if(data[i].Ticketinfo == "免費參觀"){
+                var freeCheck;
+                freeCheck = '<p><img src="./assets/icons_tag.png">'+data[i].Ticketinfo+'</p>';
+            }else{
+                freeCheck = '<p><img src="./assets/icons_tag.png">'+'以現場為主'+'</p>';;
+            };
             str += '<div class="card">';
             str += '<div class="area-location">';
             str += '<div class="area-location-img" style="background-image:url('+url+');">';
+            str+='<div class="area-name">';
             str += '<h3>'+data[i].Name+'</h3>';
             str += '<p>'+data[i].Zone+'</p></div>';
-            str += '<div class="area-location-info">'
-            str +='<ul>'
+            str+='</div>';
+            str += '<div class="area-location-info">';
+            str +='<ul>';
             str +='<li><img src="./assets/icons_clock.png">'+data[i].Opentime+'</li>';
             str +='<li><img src="./assets/icons_pin.png">'+data[i].Add+'</li>';
             str +='<li><img src="./assets/icons_phone.png">'+data[i].Tel+'</li>';
             str +='</ul>';
             str +='</div>';
-            str += '<div class="area-location-free">';
-            str += '<p><img src="./assets/icons_tag.png">'+data[i].Ticketinfo+'</p>'        
+            str += '<div class="area-location-forFree">';
+            str += freeCheck;
             str += '</div>';
             str += '</div>';
             str += '</div>';
